9c4d685e27f66f6585b917068dc095beacfe4d37
Quentin Thuet <quentin.thuet@gmail.com>
2022-06-11 16:48:24 +0200

    modification du réseau et normalisation de la matrice de coulomb

---
+++ net_dinguerize.py
@@ -0,0 +1,47 @@
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Conv(nn.Module):
+    def __init__(self):
+        super(Conv, self).__init__()
+
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=16, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, padding=1)
+        self.conv3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1)
+        self.conv4 = nn.Conv2d(in_channels=64, out_channels=96, kernel_size=3, padding=1)
+
+        self.maxpool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.flatten = nn.Flatten()
+
+        self.fc1 = nn.Linear(96, 200)
+        self.fc2 = nn.Linear(200, 100)
+        self.fc3 = nn.Linear(100, 1)
+
+    def forward(self, positions):
+        positions = F.relu(self.conv1(positions))
+        positions = self.maxpool(positions)
+        positions = F.relu(self.conv2(positions))
+        positions = self.maxpool(positions)
+        positions = F.relu(self.conv3(positions))
+        positions = self.maxpool(positions)
+        positions = F.relu(self.conv4(positions))
+        positions = self.maxpool(positions)
+        positions = self.flatten(positions)
+
+        positions_cat = positions
+
+        positions_cat = F.relu(self.fc1(positions_cat))
+        # positions = F.dropout(positions, 0.5) #dropout was included to combat overfitting
+        positions_cat = F.relu(self.fc2(positions_cat))
+        # positions = F.dropout(positions, 0.5)
+        positions_cat = self.fc3(positions_cat)
+        return positions_cat
+
+
+if __name__ == '__main__':
+    x = torch.rand(1, 15, 3)
+    net = Conv()
+    y = net(x)
---
+++ train_net_dinguerize.py
@@ -0,0 +1,159 @@
+import argparse
+from statistics import mean
+
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+from torch.utils.tensorboard import SummaryWriter
+from tqdm import tqdm
+
+import numpy as np
+
+import pickle
+
+import pandas as pd
+
+from net_dinguerize import Conv
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+class MyDataset(Dataset):
+ 
+    def __init__(self,file_name, start_split = 0, end_split = -1, transform = None, is_test = False):
+
+        with open(file_name, "rb") as file:
+            df = pickle.load(file)
+    
+        self.is_test = is_test
+        self.x_pos = df.iloc[start_split:end_split,0].values
+        if not self.is_test:
+            self.y = df.iloc[start_split:end_split,1].values
+
+        min_list = []
+        max_list = []
+        for pos in self.x_pos:
+            min_list.append(np.min(pos))
+            max_list.append(np.max(pos))
+
+        if transform is None:
+            self.transform = transforms.Normalize(np.min(min_list), np.max(max_list)-np.min(min_list))
+        else:
+            self.transform = transform
+ 
+    def __len__(self):
+        return len(self.x_pos)
+   
+    def __getitem__(self,idx):
+        return_array = [self.transform(torch.tensor([self.x_pos[idx].reshape(23,23)],dtype=torch.float32))]
+        if not self.is_test:
+            return_array.append(torch.tensor([self.y[idx]],dtype=torch.float32))
+        return return_array
+
+def train(net, optimizer, loader, epochs=10):#, writer, epochs=10):
+    criterion = nn.MSELoss()
+    for epoch in range(epochs):
+        running_loss = []
+        t = tqdm(loader)
+        for x_pos, y in t:
+            x_pos, y = x_pos.to(device), y.to(device)
+            outputs = net(x_pos)
+            loss = criterion(outputs, y)
+            running_loss.append(loss.item())
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            t.set_description(f'epoch : {epoch:2d} | training loss: {mean(running_loss):.16f}')
+        #writer.add_scalar('training loss', mean(running_loss), epoch)
+
+def test(model, dataloader):
+    cur_MSE = 0
+    with torch.no_grad():
+        for x_pos, y in dataloader:
+            x_pos, y = x_pos.to(device), y.to(device)
+            y_hat = model(x_pos)
+            cur_MSE += nn.MSELoss()(y_hat, y)
+
+    return cur_MSE/len(dataloader)
+
+def evaluate(model, dataloader, index_range = None):
+
+    y_hat = []
+    with torch.no_grad():
+        for x_pos in dataloader:
+            x_pos = x_pos[0].to(device)
+            y_hat += model(x_pos).detach().tolist()
+
+    if index_range is None:
+        ids = range(len(y_hat))
+    else:
+        ids = range(index_range[0],index_range[1]+1)
+
+    results = pd.DataFrame({"id": list(ids), "predicted": np.ravel(y_hat)[1:]})
+
+    return results
+
+if __name__=='__main__':
+
+    # TODO: writer = SummaryWriter(f'runs/MNIST')
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--exp_name', type=str, default='kaggle', help='experiment name')
+    parser.add_argument('--batch_size', type=int, default=8, help='batch size')
+    parser.add_argument('--lr', type=float, default=0.001, help='learning rate')
+    parser.add_argument('--epochs', type=int, default=50, help='training epochs')
+
+    args = parser.parse_args()
+    exp_name = args.exp_name
+    epochs = args.epochs
+    batch_size = args.batch_size
+    lr = args.lr
+
+    # datasets
+    traindata = MyDataset('train_preprocess_coulomb.pkl', end_split=5336)
+    testdata = MyDataset('train_preprocess_coulomb.pkl', start_split=5336, transform=traindata.transform)
+    truetestdata = MyDataset('truetest_preprocess_coulomb.pkl', transform=traindata.transform, is_test=True)
+
+    # dataloaders
+    trainloader = DataLoader(traindata, batch_size=batch_size, shuffle=True)
+    testloader = DataLoader(testdata, batch_size=batch_size, shuffle=True)
+    truetestloader = DataLoader(truetestdata, batch_size=batch_size, shuffle=False)
+
+    net = Conv()
+
+    # setting net on device(GPU if available, else CPU)
+    net = net.to(device)
+    #optimizer = optim.SGD(net.parameters(), lr=lr, momentum=0.9)
+    optimizer = optim.Adam(net.parameters(), lr=lr)
+
+    train(net, optimizer, trainloader, epochs)#, writer, epochs)
+    test_acc = test(net, testloader)
+    print(f'Test accuracy:{test_acc}')
+    torch.save(net.state_dict(), "mnist_net.pth")
+
+
+    truetestdata = evaluate(net, truetestloader, index_range=[6771,8462])
+
+    truetestdata.to_csv('truetest_pred.csv', index=False)
+
+    #y_hat = model(x_pos, x_num)
+
+    # # add embeddings to tensorboard
+    # perm = torch.randperm(len(trainset.data))
+    # images, labels = trainset.data[perm][:256], trainset.targets[perm][:256]
+    # images = images.unsqueeze(1).float().to(device)
+    # with torch.no_grad():
+    #     embeddings = net.get_features(images)
+    #     writer.add_embedding(embeddings,
+    #                          metadata=labels,
+    #                          label_img=images, global_step=1)
+
+    # # save networks computational graph in tensorboard
+    # writer.add_graph(net, images)
+    # # save a dataset sample in tensorboard
+    # img_grid = torchvision.utils.make_grid(images[:64])
+    # writer.add_image('mnist_images', img_grid)
+
